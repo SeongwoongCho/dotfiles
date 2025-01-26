429dfbbe9ea795e07c9a2c801c8f21f8c6bff2f7
seongwoongcho <seongwoongjo@kaist.ac.kr>
2025-01-26 21:56:49 +0900

    vim flake8, ipdb fix

--- install.sh
+++ install.sh
@@ -61,11 +61,13 @@ mkdir -p $HOME/.vim/autoload $HOME/.vim/bundle && curl -LSso $HOME/.vim/autoload
 git clone https://github.com/scrooloose/nerdtree.git $HOME/.vim/bundle/nerdtree
 ## jedi-vim: jumps, auto-suggestions/completions
 git clone --recursive https://github.com/davidhalter/jedi-vim.git $HOME/.vim/bundle/jedi-vim
+# vim-flake8
+mkdir -p ~/.vim/pack/flake8/start/
+cd ~/.vim/pack/flake8/start/
+git clone https://github.com/nvie/vim-flake8.git
 ## airline: status/tabline customization
 git clone https://github.com/vim-airline/vim-airline $HOME/.vim/bundle/vim-airline
 git clone https://github.com/vim-airline/vim-airline-themes $HOME/.vim/bundle/vim-airline-themes
-## vim-flake8 : pep8 checker
-git clone https://github.com/nvie/vim-flake8.git $HOME/.vim/bundle/vim-flake8
 ## vim-commentary
 mkdir -p $HOME/.vim/pack/tpope/start
 git clone https://tpope.io/vim/commentary.git $HOME/.vim/pack/tpope/start/commentary
