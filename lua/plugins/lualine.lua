a630b8fba8c114459d3a02a739f37938eb26c279
seongwoong <seongwoong@mobilint.com>
2025-07-26 18:15:12 +0900

    fix lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -80,7 +80,7 @@ return {
     winbar = {
       lualine_a = {},
       lualine_b = {},
-      lualine_c = { { 'filename', path = 1 } },
+      lualine_c = {},
       lualine_x = {},
       lualine_y = {},
       lualine_z = {},
@@ -88,7 +88,7 @@ return {
     inactive_winbar = {
       lualine_a = {},
       lualine_b = {},
-      lualine_c = { { 'filename', path = 1 } },
+      lualine_c = {},
       lualine_x = {},
       lualine_y = {},
       lualine_z = {},
