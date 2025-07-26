3fdc3047485d31abeecaa2e33400c0762c4a4eaf
seongwoong <seongwoong@mobilint.com>
2025-07-26 18:35:44 +0900

    gitsigns

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -15,5 +15,28 @@ return {
       delay = 0,
       ignore_whitespace = false,
     },
+    on_attach = function(bufnr)
+      local gs = package.loaded.gitsigns
+
+      local function map(mode, l, r, opts)
+        opts = opts or {}
+        opts.buffer = bufnr
+        vim.keymap.set(mode, l, r, opts)
+      end
+
+      -- Navigation
+      map('n', ']c', function()
+        if vim.wo.diff then return ']c' end
+        vim.schedule(function() gs.next_hunk() end)
+        return '<Ignore>'
+      end, { desc = 'Go to next hunk' })
+
+      map('n', '[c', function()
+        if vim.wo.diff then return '[c' end
+        vim.schedule(function() gs.prev_hunk() end)
+        return '<Ignore>'
+      end, { desc = 'Go to previous hunk' })
+
+    end,
   },
 }
--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -58,8 +58,8 @@ return {
       lualine_b = { 'branch', 'diff', 'diagnostics' },
       lualine_c = { { 'filename', path = 1 } },
       lualine_x = { 'encoding', 'fileformat', 'filetype' },
-      lualine_y = { 'progress' },
-      lualine_z = { 'location' },
+      lualine_y = { 'progress'},
+      lualine_z = { 'location'},
     },
     inactive_sections = {
       lualine_a = {},
