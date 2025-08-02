fbdcbab65be673525b0fd71e508d052c8da60eb1
seongwoong <seongwoong@mobilint.com>
2025-08-02 15:02:48 +0900

    try to set windsurf

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -1,8 +1,9 @@
-return{
+return {
     {
         'hrsh7th/nvim-cmp',
+        commit = "6c84bc75c64f778e9f1dcb798ed41c7fcb93b639",
         lazy = false,
-        priority = 100,
+        priority = 1000,
         dependencies = {
             'neovim/nvim-lspconfig',
             'hrsh7th/cmp-buffer',
@@ -12,8 +13,10 @@ return{
             'hrsh7th/cmp-cmdline',
             'onsails/lspkind.nvim',
             'ray-x/cmp-treesitter',
-            'L3MON4D3/LuaSnip', 
-            'petertriho/cmp-git'
+            'L3MON4D3/LuaSnip',
+            'saadparwaiz1/cmp_luasnip',
+            'petertriho/cmp-git',
+            -- 'Exafunction/windsurf.vim'
         },
         event = "InsertEnter",
         config = function()
@@ -34,11 +37,40 @@ return{
                 formatting = {
                     format = require 'lspkind'.cmp_format {
                         mode = "symbol_text",
+                        symbol_map = {
+                            Text = "󰉿",
+                            Method = "󰆧",
+                            Function = "󰊕",
+                            Constructor = "",
+                            Field = "󰜢",
+                            Variable = "󰀫",
+                            Class = "󰠱",
+                            Interface = "",
+                            Module = "",
+                            Property = "󰜢",
+                            Unit = "󰑭",
+                            Value = "󰎠",
+                            Enum = "",
+                            Keyword = "󰌋",
+                            Snippet = "",
+                            Color = "󰏘",
+                            File = "󰈙",
+                            Reference = "󰈇",
+                            Folder = "󰉋",
+                            EnumMember = "",
+                            Constant = "󰏿",
+                            Struct = "󰙅",
+                            Event = "",
+                            Operator = "󰆕",
+                            TypeParameter = "",
+                            Codeium = "󰚩",
+                        },
                         menu = {
                             nvim_lsp = "[LSP]",
                             buffer = "[Buffer]",
                             latex_symbols = "[Latex]",
                             luasnip = "[LuaSnip]",
+                            Codeium = "[AI]",
                         }
                     }
                 },
@@ -80,27 +112,32 @@ return{
                     end, { 'i', 's' }),
                 }),
                 sources = cmp.config.sources({
-                    { name = 'nvim_lsp' },
-                    { name = 'luasnip' },
-                    { name = 'buffer' },
-                    { name = 'calc' },
-                    { name = 'path' },
-                    { name = 'treesitter' },
-                    { name = 'git' },
+                    { name = 'nvim_lsp',   priority = 1000 },
+                    { name = 'codeium',    priority = 800 },
+                    { name = 'luasnip',    priority = 750 },
+                    { name = 'buffer',     priority = 500 },
+                    { name = 'path',       priority = 250 },
+                    { name = 'treesitter', priority = 300 },
+                    { name = 'git',        priority = 200 },
                 })
             }
 
-            -- local neocodeium = require("neocodeium")
-            -- local commands = require("neocodeium.commands")
-            -- cmp.event:on("menu_opened", function()
-            --     neocodeium.clear()
-            -- end)
-            --
-            -- neocodeium.setup({
-            --     filter = function()
-            --         return not cmp.visible()
-            --     end,
-            -- })
+            -- Setup cmdline completion
+            cmp.setup.cmdline({ '/', '?' }, {
+                mapping = cmp.mapping.preset.cmdline(),
+                sources = {
+                    { name = 'buffer' }
+                }
+            })
+
+            cmp.setup.cmdline(':', {
+                mapping = cmp.mapping.preset.cmdline(),
+                sources = cmp.config.sources({
+                    { name = 'path' }
+                }, {
+                    { name = 'cmdline' }
+                })
+            })
         end
     }
 
