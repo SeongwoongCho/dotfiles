5959f896222153e0861f275f92979aef29793404
seongwoong <seongwoong@mobilint.com>
2025-08-04 10:31:38 +0900

    update jedi_language_server to mason-lspconfig

--- lua/plugins/mason-lspconfig.lua
+++ lua/plugins/mason-lspconfig.lua
@@ -9,27 +9,9 @@ return {
         require("mason").setup()
 
         require("mason-lspconfig").setup({
-            ensure_installed = { "clangd", "lua_ls", "pylsp" }, -- 원하는 서버 나열
-            automatic_installation = true,                      -- old automatic_enable 대체
+            ensure_installed = { "clangd", "lua_ls", "pylsp", "jedi_language_server" }, -- 원하는 서버 나열
+            automatic_installation = true,                                              -- old automatic_enable 대체
         })
-
-        -- jedi-language-server 자동 설치 함수
-        local function ensure_jedi()
-            if vim.fn.executable("jedi-language-server") == 0 then
-                vim.notify("⚙️  jedi-language-server가 설치되어 있지 않아 pip으로 설치합니다...", vim.log.levels.INFO)
-                -- 시스템에 따라 pip3 / python3 -m pip 중 하나 선택하세요
-                local cmd = "python3 -m pip install --user jedi-language-server"
-                -- 또는 local cmd = "pip3 install --user jedi-language-server"
-                local result = os.execute(cmd)
-                if result ~= 0 then
-                    vim.notify("❗️ jedi-language-server 설치에 실패했습니다. 수동으로 설치해주세요.", vim.log.levels.ERROR)
-                else
-                    vim.notify("✅ jedi-language-server 설치 완료!", vim.log.levels.INFO)
-                end
-            end
-        end
-
-        ensure_jedi()
 
         local sign = function(opts)
             vim.fn.sign_define(opts.name, {
