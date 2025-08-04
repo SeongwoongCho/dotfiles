2b16345cd0ead624c359ef32cfd6786937bbe745
seongwoong <seongwoong@mobilint.com>
2025-08-04 21:50:48 +0900

    increase formatter lsp timeout from 500ms -> 2000 ms

--- lua/plugins/formatter.lua
+++ lua/plugins/formatter.lua
@@ -3,7 +3,7 @@ return {
     opts = {
         notify_on_error = true,
         format_on_save = {
-            timeout_ms = 500,
+            timeout_ms = 2000,
             lsp_fallback = true,
         },
         formatters_by_ft = {
