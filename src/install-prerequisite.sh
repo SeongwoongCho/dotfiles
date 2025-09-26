2db0b57ada0040a8fdcdcde1c7dcc8f19d21c595
seongwoong <seongwoong@mobilint.com>
2025-09-26 15:21:28 +0900

    add jedi and pylsp

--- src/install-prerequisite.sh
+++ src/install-prerequisite.sh
@@ -1,8 +1,8 @@
-###### APT ##### 
-apt-get update;
+###### APT #####
+apt-get update
 apt-get install -y software-properties-common curl
-add-apt-repository -y ppa:neovim-ppa/unstable 
-apt-get update;
+add-apt-repository -y ppa:neovim-ppa/unstable
+apt-get update
 curl -s https://deb.nodesource.com/setup_20.x | bash
 # install cargo
 curl https://sh.rustup.rs -sSf | sh -s -- -y
@@ -11,15 +11,15 @@ if ! command -v tree-sitter >/dev/null 2>&1; then
     cargo install --locked tree-sitter-cli # for latex parser
 fi
 
-DEBIAN_FRONTEND=noninteractive apt-get install -y sudo python3-opencv aria2 gcc cmake libgl1 libglib2.0-0 g++ ccache nodejs 
+DEBIAN_FRONTEND=noninteractive apt-get install -y sudo python3-opencv aria2 gcc cmake libgl1 libglib2.0-0 g++ ccache nodejs
 DEBIAN_FRONTEND=noninteractive apt-get install -y unzip zip zsh wget curl git htop libgl1 libglib2.0-0 rsync fzf
 DEBIAN_FRONTEND=noninteractive apt-get install -y tmux libevent-dev ncurses-dev bison locales chafa pkg-config build-essential libreadline-dev ripgrep fd-find
-DEBIAN_FRONTEND=noninteractive apt-get install -y clang-format clang clangd clangd-12 libomp-14-dev gdb
+DEBIAN_FRONTEND=noninteractive apt-get install -y shfmt, clang-format clang clangd clangd-12 libomp-14-dev gdb
 DEBIAN_FRONTEND=noninteractive apt-get install -y python3-venv
 cargo install git-delta
 
 # vscode cpp extension: https://github.com/microsoft/vscode-cpptools/releases
-wget https://github.com/microsoft/vscode-cpptools/releases/download/v1.24.1/cpptools-linux-x64.vsix 
+wget https://github.com/microsoft/vscode-cpptools/releases/download/v1.24.1/cpptools-linux-x64.vsix
 unzip cpptools-linux-x64.vsix -d cpptools-linux-x64/
 rm -r cpptools-linux-x64.vsix
 
@@ -30,8 +30,8 @@ cd ~/.neovim && CC=gcc CXX=g++ make CMAKE_BUILD_TYPE=RelWithDebInfo
 make install
 cd ~/.dotfiles
 
-# # ###### TMUX ##### 
-# echo **** Installing Latest tmux from the source **** 
+# # ###### TMUX #####
+# echo **** Installing Latest tmux from the source ****
 # apt-get remove -y tmux
 #
 # ## tmux
@@ -51,7 +51,7 @@ make linux test
 make install
 cd ../.. && rm -r lua_build
 
-echo **** Installing Luarocks **** 
+echo **** Installing Luarocks ****
 mkdir lua_build && cd lua_build
 wget https://luarocks.org/releases/luarocks-3.11.1.tar.gz
 tar zxpf luarocks-3.11.1.tar.gz
@@ -60,14 +60,12 @@ cd luarocks-3.11.1
 luarocks install luasocket
 cd ../.. && rm -r lua_build
 
-
-###### Installing Kitty backend & magick for image visualization (image.nvim) in vim ##### 
+###### Installing Kitty backend & magick for image visualization (image.nvim) in vim #####
 curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
 luarocks --lua-version=5.1 install magick
 DEBIAN_FRONTEND=noninteractive apt-get install -y imagemagick libmagickwand-dev
 
-
-###### PIP ##### 
+###### PIP #####
 pip install uv
 uv self update
 
@@ -81,3 +79,5 @@ uv pip install --system gpustat
 # precommit & formmater
 uv pip install --system pre-commit
 uv pip install --system black
+uv pip install --system jedi_language_server
+uv pip install --system python-lsp-server
