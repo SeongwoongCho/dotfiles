65554418a95db9cb83fcf2072498533d0c02e0b2
seongwoong <seongwoong@mobilint.com>
2025-09-26 15:19:14 +0900

    add formatter for bash

--- lua/plugins/formatter.lua
+++ lua/plugins/formatter.lua
@@ -3,10 +3,12 @@ return {
     opts = {
         notify_on_error = true,
         format_on_save = {
-            timeout_ms = 2000,
+            timeout_ms = 5000,
             lsp_fallback = true,
         },
         formatters_by_ft = {
+            bash = { "shfmt" },
+            sh = { "shfmt" },
             lua = { 'stylua' },
             python = { 'black' },
             cpp = { 'clang-format' },
