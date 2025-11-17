2b4af790375db7c0e48e4165918e0efb6cef3125
seongwoong <seongwoong@mobilint.com>
2025-11-17 13:47:48 +0900

    vim replacement aliasing

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -35,7 +35,31 @@ vim.keymap.set("n", "<leader>v", "o__import__('ipdb').set_trace(context=15)<Esc>
 -- go to the previous line after go-to-deifinition
 vim.keymap.set("n", "<leader>g", "<C-o>", { desc = "Go to previous location" })
 
--- yanking  
+-- yanking
 vim.keymap.set("n", "y", '"+y', { noremap = true })
 vim.keymap.set("n", "yy", '"+yy', { noremap = true })
 vim.keymap.set("v", "y", '"+y', { noremap = true })
+
+-- Visual mode: search and replace selected text in entire file
+local function visual_search_replace()
+    -- Get the visually selected text
+    vim.cmd('noau normal! "vy"')
+    local search_text = vim.fn.getreg("v")
+
+    -- Escape special characters for search pattern
+    search_text = vim.fn.escape(search_text, '/\\')
+
+    -- Prompt for replacement text
+    local replace_text = vim.fn.input("Replace with: ")
+    if replace_text == "" then
+        return
+    end
+
+    -- Escape special characters for replacement text
+    replace_text = vim.fn.escape(replace_text, '/\\&~')
+
+    -- Execute the substitution
+    vim.cmd(string.format("%%s/%s/%s/g", search_text, replace_text))
+end
+
+vim.keymap.set("x", "<leader>s", visual_search_replace, { desc = "Search and replace selected text in entire file" })
